df670df7de2598416fb51a95704edc1dacafdf0d
Saper140 <stuge28@gmail.com>
2024-04-19 15:23:42 -0400

    Update install.ps1
    added elivate and forced move and install

--- install.ps1
+++ install.ps1
@@ -1,4 +1,46 @@
-Invoke-WebRequest https://downloads.rclone.org/rclone-current-windows-amd64.zip -OutFile C:/Windows/system32/rclone-current-windows-amd64.zip -Force
-Expand-Archive -Path C:/Windows/system32/rclone-current-windows-amd64.zip -DestinationPath C:/Windows/system32/rclone-current-windows-amd64.zip
-Move-Item "$(((Get-ChildItem rclone*amd64).FullName).ToString())/rclone.exe" "C:/Windows/system32"
+# Get the ID and security principal of the current user account
+ $myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+ $myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+ # Get the security principal for the Administrator role
+ $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+ # Check to see if we are currently running "as Administrator"
+ if ($myWindowsPrincipal.IsInRole($adminRole))
+    {
+    # We are running "as Administrator" - so change the title and background color to indicate this
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+    clear-host
+    }
+ else
+    {
+    # We are not running "as Administrator" - so relaunch as administrator
+
+    # Create a new process object that starts PowerShell
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+    # Specify the current script path and name as a parameter
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
+
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+
+    # Exit from the current, unelevated, process
+    exit
+    }
+
+$TEMPPATH = "${env:TEMP}"
+
+Invoke-WebRequest https://downloads.rclone.org/rclone-current-windows-amd64.zip -OutFile "${TEMPPATH}\rclone-current-windows-amd64.zip"
+
+Expand-Archive -Path "${TEMPPATH}\rclone-current-windows-amd64.zip" -DestinationPath "${TEMPPATH}" -Force
+
+Move-Item "$(((Get-ChildItem "${TEMPPATH}\rclone*amd64").FullName).ToString())/rclone.exe" "C:/Windows/system32" -Force
+
 & rclone --version
+
+Read-Host
