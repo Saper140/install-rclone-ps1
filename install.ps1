c44d952b8f65fe271e0ce87bf270cb6d7d6db7e5
Saper140 <stuge28@gmail.com>
2024-04-19 14:48:52 -0400

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,4 @@
+Invoke-WebRequest https://downloads.rclone.org/rclone-current-windows-amd64.zip -OutFile C:/Windows/system32/rclone-current-windows-amd64.zip
+Expand-Archive -Path C:/Windows/system32/rclone-current-windows-amd64.zip -DestinationPath C:/Windows/system32/rclone-current-windows-amd64.zip
+Move-Item "$(((Get-ChildItem rclone*amd64).FullName).ToString())/rclone.exe" "C:/Windows/system32"
+& rclone --version
